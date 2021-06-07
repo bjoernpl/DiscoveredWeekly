27a220c64b4cd675d412d7fb30f94944ac7e20a4
Björn <bjoern.pl@outlook.de>
2021-06-07 11:35:31 +0200

    added stuff

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+    return render_template("main.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
