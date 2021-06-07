5e2fece4c87915327e3e0678fd9f088a77b00aac
Björn <bjoern.pl@outlook.de>
2021-06-07 13:30:55 +0200

    added client id

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,8 +11,16 @@ def main():
 
 @app.route("/login")
 def login():
-    return "You are trying to log in!"
+    id = os.environ.get("SPOTIFY_CLIENT_ID")
+    return f"You are trying to log in! client id: {id}"
 
+@app.route("/save_playlists", methods = ['POST'])
+def save_playlists():
+    code = request.headers.get("passcode", "placeholder")
+    if code == os.environ.get("SAVE_PLAYLISTS_CODE"):
+        return "Great success"
+    else:
+        return "You should not be here. Shoo"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
