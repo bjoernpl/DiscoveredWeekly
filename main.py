013817d49a47dceb3c0f4220657c3e1f2316f764
Björn <bjoern.pl@outlook.de>
2021-06-08 15:58:35 +0200

    added correct auth flow

--- main.py
+++ main.py
@@ -15,21 +15,44 @@ firebase_admin.initialize_app(cred, {
 })
 
 db = firestore.client()
+auth = SpotifyOAuth(
+        client_id=os.environ.get("SPOTIFY_CLIENT_ID", "none"), 
+        client_secret=os.environ.get("SPOTIFY_CLIENT_SECRET", "none"), 
+        redirect_uri="https://pluester.dev/logged_in",
+        scope="playlist-modify-private playlist-read-private user-library-read")
+    
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    name = os.environ.get("NAME", "World")
-    return render_template("main.html")
+    auth_url = auth.get_authorize_url()
+    return render_template("main.html", auth_url=auth_url)
 
-@app.route("/login")
-def login():
-    sp = authorize_spotify()
-    return sp.current_user()
 
 @app.route("/logged_in")
 def logged_in():
-    return "this is a test"
+    access_token = ""
+    token_info = auth.get_cached_token()
+
+    if token_info:
+        print("Found cached token!")
+        access_token = token_info['access_token']
+    else:
+        url = request.url
+        code = auth.parse_response_code(url)
+        if code:
+            print("Found Spotify auth code in Request URL! Trying to get valid access token...")
+            token_info = auth.get_access_token(code)
+            access_token = token_info['access_token']
+
+    if access_token:
+        print("Access token available! Trying to get user information...")
+        sp = spotipy.Spotify(access_token)
+        results = sp.current_user()
+        return results
+
+    else:
+        return "test :D"
 
 @app.route("/users")
 def get_users():
@@ -54,12 +77,7 @@ def save_playlists():
         return "You should not be here. Shoo"
 
 def authorize_spotify():
-    token = SpotifyOAuth(
-        client_id=os.environ.get("SPOTIFY_CLIENT_ID", "none"), 
-        client_secret=os.environ.get("SPOTIFY_CLIENT_SECRET", "none"), 
-        redirect_uri="https://pluester.dev/logged_in",
-        scope="playlist-modify-private playlist-read-private user-library-read")
-
+    
     return spotipy.Spotify(auth_manager=token)
 
 if __name__ == "__main__":
